1deba19371ffcef3ebd9a69638f50c4fdcae468a
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2023-11-13 07:16:47 +0800

    Add code to convert to utf8

--- try_read_quiz_qns.py
+++ try_read_quiz_qns.py
@@ -1,3 +1,17 @@
-with open("quiz_questions.txt", encoding='utf-8') as stream:
-    for line in stream.read().splitlines():
-        print(line);
+INPUT_PATH = "quiz_questions.txt";
+
+input_stream = open(INPUT_PATH, encoding='cp1252');
+input_text = input_stream.read();
+print(input_text);
+input_stream.close();
+input_stream = None;
+
+OUTPUT_PATH = "quiz_questions_utf8.txt";
+
+output_stream = open(OUTPUT_PATH, "w", encoding='utf-8')
+output_stream.write(input_text);
+output_stream.close();
+
+input_stream = open(OUTPUT_PATH);
+input_text = input_stream.read();
+print(input_text);
